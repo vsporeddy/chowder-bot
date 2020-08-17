d8b3d8ea6d0d75facc8f97aa52c074f35044a6f5
vsporeddy <vs.poreddy@gmail.com>
2020-08-16 19:29:11 -0700

    lint

--- games/hangman/hangman.py
+++ games/hangman/hangman.py
@@ -9,6 +9,7 @@ from chowder import chowder
 with open("games/hangman/hangman_config.json", "r") as read_file:
     config = json.load(read_file)
 
+
 async def start(bot, ctx, players):
     await ctx.send(f"Starting a game of **hangman** with {', '.join([p.mention for p in players])}")
     word = get_word()
@@ -18,6 +19,7 @@ async def start(bot, ctx, players):
     else:
         await ctx.send(get_defeat_message().format(word=word))
     return victory
+
 
 async def play(bot, ctx, players, word):
     wordset = set(word.replace(" ", ""))
@@ -51,6 +53,7 @@ async def play(bot, ctx, players, word):
 
     return False
 
+
 def get_display_word(word, guesses):
     letters = ""
     for letter in word:
@@ -59,33 +62,39 @@ def get_display_word(word, guesses):
         elif letter in guesses:
             letters += f"{letter} "
         else:
-            letters += f"__ __ "
+            letters += "__ __ "
     return letters
+
 
 async def display(ctx, word, guesses, strikes, players, title):
     embed = discord.Embed(
-            title = get_display_word(word, guesses),
-            color = discord.Colour.dark_red()
+            title=get_display_word(word, guesses),
+            color=discord.Colour.dark_red()
         )
     guess_str = ', '.join(guesses) if guesses else config["emote"]
-    embed.set_image(url = config["gallows"][strikes])
-    embed.set_author(name = title)
+    embed.set_image(url=config["gallows"][strikes])
+    embed.set_author(name=title)
     embed.add_field(name="Guesses", inline=True, value=guess_str)
     embed.add_field(name="Strikes left", inline=True, value=strikes)
     embed.set_footer(text=f"Players: {players}")
     await ctx.send(embed=embed)
 
+
 def get_word():
     return random.choice(config["words"])
+
 
 def get_title():
     return random.choice(config["titles"])
 
+
 def get_victory_message():
     return random.choice(config["victory_messages"])
 
+
 def get_defeat_message():
     return random.choice(config["defeat_messages"])
+
 
 def get_strike_message():
     return random.choice(config["strike_messages"])
