44d3e3da876a7dcfd7a8ae96fd9fa4c241124f85
vsporeddy <vs.poreddy@gmail.com>
2022-02-04 20:55:25 -0800

    Mention user for rolls (#61)

--- games/gamble/gamble.py
+++ games/gamble/gamble.py
@@ -18,12 +18,11 @@ class Gamble(commands.Cog):
 
     @commands.command(name="roll", brief="Woll dat shit", aliases=["woll", "wolldatshit"])
     async def roll(self, ctx, max_roll: int = 6):
-        name = chowder.get_name(ctx.author)
         roll_value = random.randint(1, max_roll)
         if roll_value >= max_roll / 2:
-            await ctx.send(f"Not bad {name}, you rolled a **{roll_value}**")
+            await ctx.send(f"Not bad {ctx.author.mention}, you rolled a **{roll_value}**")
         else:
-            await ctx.send(f"Get rekt {name}, you rolled a **{roll_value}**")
+            await ctx.send(f"Get rekt {ctx.author.mention}, you rolled a **{roll_value}**")
 
     @commands.command(name="flip", brief="Flip a coin", aliases=["coin", "flipdatshit"])
     async def flip(self, ctx):
