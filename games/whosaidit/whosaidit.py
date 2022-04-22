50f7457d613867b5d8ae6aaa82c4c1806ae422cc
vsporeddy <vs.poreddy@gmail.com>
2022-04-21 21:04:43 -0700

    convert set to list to shuffle

--- games/whosaidit/whosaidit.py
+++ games/whosaidit/whosaidit.py
@@ -53,7 +53,7 @@ async def play(bot, ctx, players):
     channel_members.remove(message_to_guess.author)
     authors.update(random.sample(channel_members, 4))
     authors.add(message_to_guess.author)
-    random.shuffle(authors)
+    random.shuffle(list(authors))
 
     choices = {choice + 1: author for choice, author in enumerate(authors)}
     answer = list(choices.keys())[list(choices.values()).index(message_to_guess.author)]
