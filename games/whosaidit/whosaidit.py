85f199fd81bf5811ff01aec08057c88051ac588b
vsporeddy <vs.poreddy@gmail.com>
2022-04-21 21:25:26 -0700

    Improve timestamp formatting

--- games/whosaidit/whosaidit.py
+++ games/whosaidit/whosaidit.py
@@ -66,7 +66,7 @@ async def play(bot, ctx, players):
         if guess == answer:
             winners.append(bot.get_user(player))
 
-    await ctx.send(f"This quote was from {message_to_guess.author.name} on {message_to_guess.created_at}!")
+    await ctx.send(f"This quote was from {message_to_guess.author.name} on <t:{message_to_guess.created_at.timestamp()}:d>!")
 
     return winners
 
